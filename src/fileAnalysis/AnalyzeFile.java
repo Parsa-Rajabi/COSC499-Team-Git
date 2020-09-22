88f9d8f8a0dd4cc893faaa5bf955c63e30770393
Parsa <ParsaRajabiPR@gmail.com>
2020-09-22 15:26:32 -0700

    added print line to AnalyzeFile.java, added tests, updated gitignore file

--- src/fileAnalysis/AnalyzeFile.java
+++ src/fileAnalysis/AnalyzeFile.java
@@ -7,9 +7,12 @@ public class AnalyzeFile {
 
     public static void analyzeFile() {
         try {
-            String text = ReadFile.readFile("src/resources/SampleText.txt");
+            String text = ReadFile.readFile("resources/SampleText.txt");
             // TODO: display analysis results here
             // ex. System.out.println("Number of words: " + countWords(text))
+
+
+
             System.out.println("Number of characters: " + charCount.countChar(text));
         } catch (Exception e) {
             e.printStackTrace();
--- src/tests/charCountTest.java
+++ src/tests/charCountTest.java
@@ -1,12 +1,14 @@
 package tests;
-import fileAnalysis.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+import fileAnalysis.charCount;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class charCountTest {
     
     @Test
-	public void countWordsShouldReturnZeroWhenTextIsEmpty() {
+	public void countCharShouldReturnZeroWhenEmptyString() {
 		
 		// Expected result
 		int expectedResult = 0;
@@ -16,6 +18,71 @@ public class charCountTest {
 		
 		// When the input string is empty text
 		int numChars = charCount.countChar(emptyText);
+
+		// Then
+		assertEquals(expectedResult, numChars);
+	}
+
+
+	@Test
+	public void countCharShouldReturnOneWhenExtraWhitespaceAdded() {
+
+		// Expected result
+		int expectedResult = 1;
+
+		// Given text
+		String givenText = "     p      ";
+
+		// When the input string is empty text
+		int numChars = charCount.countChar(givenText);
+
+		// Then
+		assertEquals(expectedResult, numChars);
+	}
+
+
+	@Test
+	public void countCharShouldReturnValueWhenStringHasMixUpperLowerCase() {
+
+		// Expected result
+		int expectedResult = 4;
+
+		// Given text
+		String givenText = "teSt";
+
+		// When the input string is empty text
+		int numChars = charCount.countChar(givenText);
+
+		// Then
+		assertEquals(expectedResult, numChars);
+	}
+
+	@Test
+	public void countCharShouldReturnValueWhenStringIsNumbers() {
+
+		// Expected result
+		int expectedResult = 6;
+
+		// Given text
+		String givenText = "450124";
+
+		// When the input string is empty text
+		int numChars = charCount.countChar(givenText);
+
+		// Then
+		assertEquals(expectedResult, numChars);
+	}
+	@Test
+	public void countCharShouldReturnValueWhenStringHasSpecialChar() {
+
+		// Expected result
+		int expectedResult = 23;
+
+		// Given text
+		String givenText = ">Welcome, to COSC 499!<";
+
+		// When the input string is empty text
+		int numChars = charCount.countChar(givenText);
 
 		// Then
 		assertEquals(expectedResult, numChars);
