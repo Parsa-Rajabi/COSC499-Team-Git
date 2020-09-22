d3495ab8c38b0c0fb762d007615356901a17661f
Jeff Hatton <lightbrite88@gmail.com>
2020-09-22 15:26:12 -0600

    Update AnalyzeFile.java
    removed merge conflict code

--- src/fileAnalysis/AnalyzeFile.java
+++ src/fileAnalysis/AnalyzeFile.java
@@ -8,14 +8,11 @@ public class AnalyzeFile {
     public static void analyzeFile() {
         try {
             String text = ReadFile.readFile("src/resources/SampleText.txt");
-<<<<<<< HEAD
             // TODO: display analysis results here
             System.out.println("Number of words: " + CountWords.countWords(text));
 
-=======
             int numOfVowels = CountVowels.countVowels(text);
             System.out.println("Number of vowels: " + numOfVowels);
->>>>>>> a9c69b5... Update AnalyzeFile.java
         } catch (Exception e) {
             e.printStackTrace();
         }
