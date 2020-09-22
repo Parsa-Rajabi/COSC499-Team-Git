e28452a163c091aef89ea5311b549a80bac0a0ec
Jeff Hatton <lightbrite88@gmail.com>
2020-09-21 22:13:52 -0600

    Created basic method structure
    Missing core algorithm.

---
+++ src/fileAnalysis/CountVowels.java
@@ -0,0 +1,18 @@
+package fileAnalysis;
+
+public class CountVowels {
+    public static int countVowels(String inputString){
+        if (inputString==null || inputString.isEmpty()) //Empty and null strings have no vowels
+            return 0;
+        else {
+            int numVowels = 0;
+            //finish counting algorithm here
+            return numVowels;
+        }
+
+
+    }
+
+
+
+}
