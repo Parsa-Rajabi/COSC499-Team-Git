eeb4486c687721045261677432aa8ea6c39c9add
QinAlex <qinzhenyu1123@gmail.com>
2020-09-21 18:18:01 -0700

    Change the format of the unitTest.

--- src/tests/CountWordsTest.java
+++ src/tests/CountWordsTest.java
@@ -9,18 +9,50 @@ import org.junit.jupiter.api.Test;
 class CountWordsTest {
 
 	@Test
-	public void testCountWords_Empty() {
-		assertEquals(0, CountWords.countWords(""));
+	public void countWordsShouldReturnZeroWhenTextIsEmpty() {
+		
+		// Expected result
+		int expectedResult = 0;
+		
+		// Given text
+		String emptyText = "";
+		
+		// When the input string is empty
+		int wordCount = CountWords.countWords(emptyText);
+
+		// Then
+		assertEquals(expectedResult, wordCount);
 	}
 	
 	@Test
-	public void testCountWords_Null() {
-		assertEquals(0, CountWords.countWords(null));
+	public void countWordsShouldReturnZeroWhenTextIsNull() {
+		// Expected result
+		int expectedResult = 0;
+				
+		// Given text
+		String nullText = null;
+			
+		// When the input string is null
+		int wordCount = CountWords.countWords(nullText);
+
+		// Then
+		assertEquals(expectedResult, wordCount);
 	}
 	
 	@Test
-	public void testCountWords_WithString() {
-		assertEquals(5, CountWords.countWords("haha lol wut is that "));
+	public void countWordsShouldReturnTheCorrectNumbersOfTextWhenTextIsGiven() {
+		
+		// Expected result
+		int expectedResult = 5;
+				
+		// Given text
+		String givenText = "haha lol wut is that ";
+				
+		// When the input string is given
+		int wordCount = CountWords.countWords(givenText);
+
+		// Then
+		assertEquals(expectedResult, wordCount);
 	}
 
 }
