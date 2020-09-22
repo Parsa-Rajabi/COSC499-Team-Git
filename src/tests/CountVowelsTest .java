6b019a91f0c9e59cc1dfd528c88cbe382b323dbb
Jeff Hatton <lightbrite88@gmail.com>
2020-09-21 23:28:14 -0600

    Update CountVowelsTest .java
    Added two more simple tests

--- src/tests/CountVowelsTest .java
+++ src/tests/CountVowelsTest .java
@@ -16,10 +16,42 @@ class CountVowelsTest {
 		String emptyText = "";
 		
 		// When the input string is empty text
-		int wordCount = CountVowels.countVowels(emptyText);
+		int vowelCount = CountVowels.countVowels(emptyText);
 
 		// Then
-		assertEquals(expectedResult, wordCount);
+		assertEquals(expectedResult, vowelCount);
     }
+
+    @Test
+	public void countVowelsShouldReturnZeroWhenTextIsNull() {
+		// Expected result
+		int expectedResult = 0;
+				
+		// Given text
+		String nullText = null;
+			
+		// When the input string is null
+		int vowelCount = CountVowels.countVowels(nullText);
+
+		// Then
+		assertEquals(expectedResult, vowelCount);
+	}
+	
+	@Test
+	public void countVowelsShouldReturnNumberOfVowelsWhenTextWithVowelsIsGiven() {
+		
+		// Expected result
+		int expectedResult = 7;
+				
+		// Given text
+		String givenText = "Whatever, this has vowels";
+				
+		// When the input string is given
+		int vowelCount = CountVowels.countVowels(givenText);
+
+		// Then
+		assertEquals(expectedResult, vowelCount);
+	}
+
     
 }
