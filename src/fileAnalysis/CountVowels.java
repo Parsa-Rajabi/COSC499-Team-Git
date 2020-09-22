1b81a6eace4660dbe64dfc31e1a19fccc68b465f
Jeff Hatton <lightbrite88@gmail.com>
2020-09-21 22:18:39 -0600

    Update CountVowels.java
    added better comments

--- src/fileAnalysis/CountVowels.java
+++ src/fileAnalysis/CountVowels.java
@@ -1,18 +1,19 @@
 package fileAnalysis;
 
 public class CountVowels {
-    public static int countVowels(String inputString){
-        if (inputString==null || inputString.isEmpty()) //Empty and null strings have no vowels
-            return 0;
-        else {
-            int numVowels = 0;
+    public static int countVowels(String inputString){ //static -> takes string, returns integer
+        if (inputString==null || inputString.isEmpty()){ //Check for empty and null strings
+            return 0; //Empty and null strings have no vowels
+        } else {
+            int numVowels = 0; // create vowel counter
+            //TODO
             //finish counting algorithm here
             return numVowels;
-        }
+        } //close conditional
 
 
-    }
+    } //close static method
 
 
 
-}
+} //close class
